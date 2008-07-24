8e0ac29d2404913c31adf7d77ce0bf5fb73b25e2
a2800276 <a2800276@5fdf77b1-4124-0410-b011-2b42da5c28b3>
2008-07-24 17:09:48 +0000

    b working and tested
    git-svn-id: https://a2800276.googlecode.com/svn/trunk/ruby/tlv@143 5fdf77b1-4124-0410-b011-2b42da5c28b3

--- test/tlv_test.rb
+++ test/tlv_test.rb
@@ -7,7 +7,7 @@ class TestTLV < Test::Unit::TestCase
   end
   
   class TLVTest < TLV
-    tag "00", "Test TLV"
+    tlv "11", "Test TLV"
     b   8,   "first field",  :first
     b   8,   "second field", :second
   end
@@ -18,7 +18,7 @@ class TestTLV < Test::Unit::TestCase
     t.second="\xAA"
     assert_equal "\x01", t.first
     assert_equal "\xaa", t.second
-    assert_equal "\x01\xaa", t.to_b
+    assert_equal "\x11\x02\x01\xaa", t.to_b
 
     assert_raise(RuntimeError) {
       t.first="\x02\x03"
@@ -32,4 +32,75 @@ class TestTLV < Test::Unit::TestCase
     puts t.to_s
     assert true
   end 
+
+  def test_parse_tag
+    bytes = "\x01\x00\x00"
+    tag, rest = TLV.get_tag bytes
+    assert_equal "\x01", tag
+    assert_equal "\x00\x00", rest
+
+    bytes = "\xFF\x00\x00"
+    tag, rest = TLV.get_tag bytes
+    assert_equal "\xff\x00", tag
+    assert_equal "\x00", rest
+
+
+    bytes = "\xFF\x85\xAA"
+    tag, rest = TLV.get_tag bytes
+    assert_equal "\xff\x85\xaa", tag
+    assert_equal "", rest
+    
+    bytes = "\x11\x02\x01\xaa"
+    tag, rest = TLV.get_tag bytes
+    assert_equal "\x11", tag
+    assert_equal "\x02\x01\xaa", rest
+  end
+  def test_parse_length
+    bytes = "\x03\x02"
+    len, rest = TLV.get_length bytes
+    assert_equal 3, len
+    assert_equal "\x02", rest
+    
+    bytes = "\x81\x02\x11\x22"
+    len, rest = TLV.get_length bytes
+    assert_equal 2, len
+    assert_equal "\x11\x22", rest
+
+
+    bytes = "\x84\x00\x00\x00\x02\x11\x22"
+    len, rest = TLV.get_length bytes
+    assert_equal 2, len
+    assert_equal "\x11\x22", rest
+
+
+    bytes = "\x83\x00\x00\x02\x11\x22"
+    len, rest = TLV.get_length bytes
+    assert_equal 2, len
+    assert_equal "\x11\x22", rest
+    
+    bytes = "\x82\x10\x01\x11\x22"
+    len, rest = TLV.get_length bytes
+    assert_equal 4097, len
+    assert_equal "\x11\x22", rest
+    
+    bytes = "\x83\x00\x10\x01\x11\x22"
+    len, rest = TLV.get_length bytes
+    assert_equal 4097, len
+    assert_equal "\x11\x22", rest
+  end
+
+  def test_parse
+    t = TLVTest.new
+    assert_equal "\x00", t.first
+    t.first="\x01"
+    t.second="\xAA"
+    
+    assert "\x01", t.first
+    bytes = t.to_b
+
+    t = TLV.parse bytes
+    assert_equal TLVTest, t.class
+    assert_equal "\x01", t.first
+    assert_equal "\xAA", t.second
+  end
 end
