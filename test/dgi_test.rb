084f0cca814cca8fdcba1306cf6e24b4ce3ed89b
a2800276 <a2800276@5fdf77b1-4124-0410-b011-2b42da5c28b3>
2008-08-07 18:37:21 +0000

    dgi tests
    git-svn-id: https://a2800276.googlecode.com/svn/trunk/ruby/tlv@164 5fdf77b1-4124-0410-b011-2b42da5c28b3

--- test/dgi_test.rb
+++ test/dgi_test.rb
@@ -7,13 +7,13 @@ class TestDGI < Test::Unit::TestCase
   end
   
   class TLVTest < DGI
-    tlv "01", "Test TLV"
+    tlv "0101", "Test TLV"
     b   8,   "first field",  :first
     b   8,   "second field", :second
   end
 
   class TLVTest2 < DGI
-    tlv "02", "Test Rubify"
+    tlv "0102", "Test Rubify"
     b   8,   "My Test"
     b   8,   "Oh M@i!"
   end
@@ -47,78 +47,12 @@ class TestDGI < Test::Unit::TestCase
   def test_basics
     t = TLVTest.new
     basics t
-    assert_equal "\x01\x02\x01\xaa", t.to_b
+    assert_equal "\x01\x01\x02\x01\xaa", t.to_b
 
-    t = TLVTestNoTag.new
-    basics t
-    assert_equal "\x01\xaa", t.to_b
   end 
 
   def test_parse_tag
-    bytes = "\x01\x00\x00"
-    tag, rest = TLV.get_tag bytes
-    assert_equal "\x01", tag
-    assert_equal "\x00\x00", rest
 
-    bytes = "\xFF\x00\x00"
-    tag, rest = TLV.get_tag bytes
-    assert_equal "\xff\x00", tag
-    assert_equal "\x00", rest
-
-
-    bytes = "\xFF\x85\xAA"
-    tag, rest = TLV.get_tag bytes
-    assert_equal "\xff\x85\xaa", tag
-    assert_equal "", rest
-    
-    bytes = "\x11\x02\x01\xaa"
-    tag, rest = TLV.get_tag bytes
-    assert_equal "\x11", tag
-    assert_equal "\x02\x01\xaa", rest
-
-
-    bytes = TLV.s2b "9f7f2aff"
-    tag, rest = TLV.get_tag bytes
-    assert_equal "\x9f\x7f", tag
-    assert_equal "\x2a\xff", rest
-
-    length, rest = TLV.get_length rest
-    assert_equal 0x2a, length
-
-
-  end
-  def test_parse_length
-    bytes = "\x03\x02"
-    len, rest = TLV.get_length bytes
-    assert_equal 3, len
-    assert_equal "\x02", rest
-    
-    bytes = "\x81\x02\x11\x22"
-    len, rest = TLV.get_length bytes
-    assert_equal 2, len
-    assert_equal "\x11\x22", rest
-
-
-    bytes = "\x84\x00\x00\x00\x02\x11\x22"
-    len, rest = TLV.get_length bytes
-    assert_equal 2, len
-    assert_equal "\x11\x22", rest
-
-
-    bytes = "\x83\x00\x00\x02\x11\x22"
-    len, rest = TLV.get_length bytes
-    assert_equal 2, len
-    assert_equal "\x11\x22", rest
-    
-    bytes = "\x82\x10\x01\x11\x22"
-    len, rest = TLV.get_length bytes
-    assert_equal 4097, len
-    assert_equal "\x11\x22", rest
-    
-    bytes = "\x83\x00\x10\x01\x11\x22"
-    len, rest = TLV.get_length bytes
-    assert_equal 4097, len
-    assert_equal "\x11\x22", rest
   end
 
   def test_length
